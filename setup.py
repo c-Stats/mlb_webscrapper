eb0c59e671c92b847259d4e6e3a4031b46cce06e
F.Fregeau <frankfredj@gmail.com>
2021-04-30 18:46:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 
     packages = ['mlb_webscrapper'],
 
-    install_requires = ["pandas", "requests",
+    install_requires = ["pandas", "requests", "bs4",
                 "numpy", "tqdm", "path"],
 
     version = '0.1',
@@ -17,3 +17,31 @@ setup(
     description = 'webscrapes MLB player stats and scores',
 
 )
+
+
+import pandas as pd
+import requests
+import urllib
+import numpy as np
+
+import os.path
+from os import path
+
+from bs4 import BeautifulSoup
+
+from datetime import datetime
+from datetime import timedelta
+
+import time
+import random
+
+from tqdm import tqdm
+
+import sys 
+
+from os import listdir
+from os.path import isfile, join
+
+import re
+
+pd.options.mode.chained_assignment = None
