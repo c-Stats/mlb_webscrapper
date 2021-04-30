b7f5b931eee6eb64a18dcd3c5467e6a167853fd2
F.Fregeau <frankfredj@gmail.com>
2021-04-30 19:53:36 -0400

    Fix

--- setup.py
+++ setup.py
@@ -1,31 +1,5 @@
 from setuptools import find_packages, setup
 
-import pandas as pd
-import requests
-import urllib
-import numpy as np
-
-import os.path
-from os import path
-
-from bs4 import BeautifulSoup
-
-from datetime import datetime
-from datetime import timedelta
-
-import time
-import random
-
-from tqdm import tqdm
-
-import sys 
-
-from os import listdir
-from os.path import isfile, join
-
-import re
-
-pd.options.mode.chained_assignment = None
 
 setup(
     name = 'mlb_webscrapper',
